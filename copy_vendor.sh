d6c0f643fb02cbaac17f2b5eeeea4e4ea453fae6
Stefan Neculai <stefan.neculai@gmail.com>
2020-07-06 17:13:12 +0300

    Add head.

---
+++ copy_vendor.sh
@@ -0,0 +1,6 @@
+cp ./node_modules/jquery/dist/jquery.min.js assets/vendor/
+cp ./node_modules/popper.js/dist/umd/popper.min.js assets/vendor/
+cp ./node_modules/bootstrap/dist/js/bootstrap.min.js assets/vendor/
+
+cp ./node_modules/popper.js/dist/umd/popper.min.js.map assets/vendor/
+cp ./node_modules/bootstrap/dist/js/bootstrap.min.js.map assets/vendor/
